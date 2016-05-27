281ba6ed0e00cb0eeb5e5316f5029330b75e9a58
Malven <marcusalven@gmail.com>
2016-05-27 08:53:10 +0200

    Fixes

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour
+{
+    float lifeTime = 15f;
+    void Update()
+    {
+        gameObject.transform.Rotate(0, 0, 50 * Time.deltaTime);
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //ADD EFFECT/SCORE WHATEVER
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+}
